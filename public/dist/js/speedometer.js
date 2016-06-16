5215ae48d9066651515943b19f851b85b83403cc
Hamza Sheikh <hsheikhm@gmail.com>
2016-06-16 01:10:47 +0100

    completed logic for random number generated on set interval

--- public/dist/js/speedometer.js
+++ public/dist/js/speedometer.js
@@ -30,13 +30,20 @@
 
   var speedometerAppControllers = angular.module('speedometerAppControllers', []);
 
-  speedometerAppControllers.controller('MainCtrl', ['$scope', function($scope){
+  speedometerAppControllers.controller('MainCtrl', ['$scope', '$interval',
+    function($scope, $interval){
 
     $scope.generateRandomNumber = function(){
-      return Math.floor(Math.random() * 100) + 1;
+      $scope.randomNumber = Math.floor(Math.random() * 100) + 1;
     };
 
-    $scope.randomNumber = $scope.generateRandomNumber();
+    $scope.generateRandomNumber();
+
+    $scope.interval = 5;
+
+    $scope.startTimer = function(){
+      $interval($scope.generateRandomNumber, 3000);
+    };
 
   }]);
 
